9b9b78cab8f895314e578fcb68299ad23032cfbc
Akshat Awasthi <95212552+Darkfur2340@users.noreply.github.com>
2022-04-04 10:31:22 +0530

    Add files via upload

---
+++ helpers/clean.py
@@ -0,0 +1,14 @@
+import shutil
+
+
+async def delete_all(root: str):
+    """
+    Delete a Folder.
+
+    :param root: Pass Folder Path as String.
+    """
+
+    try:
+        shutil.rmtree(root)
+    except Exception as e:
+        print(e)
